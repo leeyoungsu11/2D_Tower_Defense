255cd5f6fd4bb8bc13b567d1088e1b6fd9b13f03
leeyoungsu11 <116151770+leeyoungsu11@users.noreply.github.com>
2023-11-21 01:24:54 +0900

    11_20

--- 2D_TD/Assets/Scripts/MoveEnemy.cs
+++ 2D_TD/Assets/Scripts/MoveEnemy.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MoveEnemy : MonoBehaviour
 {
+    Animator animator;
+
     [SerializeField]
     private float moveSpeed=0.0f;
     [SerializeField]
@@ -13,9 +15,29 @@ public class MoveEnemy : MonoBehaviour
     void Update()
     {
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+        
     }
     public void moveTo(Vector3 dir)
     {
+        Debug.Log(dir);
         moveDir = dir;
+        if(dir.y < 0)
+        {
+            //animator.SetTrigger("D");
+        }
+        if(dir.x > 0)
+        {
+            animator.SetTrigger("R");
+            
+        }
+        else if(dir.y > 0 )
+        {
+            animator.SetTrigger("U");
+        }
+    }
+
+    void Start() 
+    {
+        animator = transform.GetComponent<Animator>();
     }
 }
