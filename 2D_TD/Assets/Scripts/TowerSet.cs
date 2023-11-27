673269d933f014962babb38acf1205e938561dde
leeyoungsu11 <116151770+leeyoungsu11@users.noreply.github.com>
2023-11-28 02:29:47 +0900

    11_27
    타워 에니메이션

---
+++ 2D_TD/Assets/Scripts/TowerSet.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerSet : MonoBehaviour
+{
+    public SpriteRenderer render;
+
+    void Start()
+    {
+        render = GetComponent<SpriteRenderer>();
+    }
+
+
+}
