046f20176c7f4ce128d5e711bc6621b897d12b4b
gycks2006 <gycks2006@gmail.com>
2023-11-28 10:28:28 +0900

    Update GameManager.cs

--- 2D_TD/Assets/Scripts/GameManager.cs
+++ 2D_TD/Assets/Scripts/GameManager.cs
@@ -43,6 +43,7 @@ public class GameManager : MonoBehaviour
         {
             TextContrl.instance.ShowGameOver();
             TextContrl.instance.ReTryBtn();
+            TextContrl.instance.NextBtn();
             Pause();
             
         }
