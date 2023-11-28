236e2c57a323360a03c5d22ebc0a41fd774697fb
gycks2006 <gycks2006@gmail.com>
2023-11-29 03:18:46 +0900

    실패

--- 2D_TD/Assets/Scripts/StarManager.cs
+++ 2D_TD/Assets/Scripts/StarManager.cs
@@ -4,12 +4,14 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class StarManager : MonoBehaviour
 {
-    public Image[] stars = new Image[0];
+    public Image[] stars;
     
-
+    
+    
     //int clearStage = 0;
 
     //public void SetStarts(int completedStages)
@@ -21,16 +23,10 @@ public class StarManager : MonoBehaviour
     //        starts[i].gameObject.SetActive(i < clearStage);
     //    }
     //}
-    // 스테이지 클리어 상태를 저장하는 함수
-    public void SetStageCleared(int stageNumber)
-    {
-        // 스테이지 클리어 상태를 저장
-        PlayerPrefs.SetInt("Stage_" + stageNumber + "_cleared", 1);
-    }
+    
+    
     private void Start()
     {
-        if (stars == null)
-        {  }
         stars[0].gameObject.SetActive(false);
         stars[1].gameObject.SetActive(false);
         stars[2].gameObject.SetActive(false);
@@ -40,15 +36,28 @@ public class StarManager : MonoBehaviour
         
     }
     // 넥스트 버튼을 눌렀을 때 클리어 상태 확인 및 별 활성화
-    public void OnNextButtonClicked()
-    {
-        // 넥스트 버튼을 눌렀을 때만 1번 씬에서 클리어 표시로써 별을 활성화
-        if (SceneManager.GetActiveScene().buildIndex == 1)
-        {
-                stars[0].gameObject.SetActive(true);
-                Debug.Log("1번 스테이지 클리어!");
-        }
+    //public void OnNextButtonClicked()
+    //{
+    //    //// 넥스트 버튼을 눌렀을 때만 1번 씬에서 클리어 표시로써 별을 활성화
+    //    //if (/*SceneManager.GetActiveScene().buildIndex == 1*/)
+    //    //{
+    //    //    stars[0].gameObject.SetActive(true);
 
-        
-    }
+    //    //    LoadingSceneController.LoadScene(1);
+    //    //}
+    //    SceneManager.GetSceneByBuildIndex(1);
+    //    LoadingSceneController.LoadScene(1);
+    //    stars[0].gameObject.SetActive(true);
+
+
+    //}
+    //public void Onpointerup(PointerEventData eventData)
+    //{
+    //    SceneManager.GetSceneByBuildIndex(1);
+    //    LoadingSceneController.LoadScene(1);
+    //    stars[0] .gameObject.SetActive(true);
+    //}
+    
+
+    
 }
